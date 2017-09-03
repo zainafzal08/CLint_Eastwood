e1bba64bb4a3f1f72c5c6442148140a89b0735a6
Zain Afzal <zain.afz@gmail.com>
2017-09-04 09:49:33 +1000

    Got basic ast building, next job: abstract way to define syntax rules

--- Parser.py
+++ Parser.py
@@ -2,6 +2,7 @@ from Lexer import Lexer
 from Lexer import Token
 from Tree import Tree
 import re
+import logging, sys
 
 class Grammar():
 	def __init__(self,tokens,spec):
@@ -68,6 +69,8 @@ class Parser():
 		self.grammar = Grammar(l.getTokenList(),spec)
 		self.lexer = l
 		self.tree = Tree()
+		self.compilerTags = []
+		self.comments = []
 	# given a transformation and a token, return the 
 	# transformation that should be taken
 	def collapseOptions(self, trans, t):
@@ -79,20 +82,21 @@ class Parser():
 				continue
 			elif t.type in self.grammar.firstSets[term]:
 				return option
-		raise Exception("Syntax error :/")
+		raise Exception("Attempt to decend into "+trans+ " failed due to mathc failure on "+t.type)
 
 	# decend into a given transformation
 	# updating the tree and terminating automatically
 	def decend(self, trans, t):
-		print("Decending into "+trans)
+		logging.debug(" Decending into "+trans)
 		option = self.collapseOptions(trans,t)
-		print("    Collapsing into "+option)
+		logging.debug("    Collapsing into "+option)
 		for term in option.split(" "):
 			if term in self.grammar.tokens and term == t.type:
+				logging.debug("    accepting "+t.type)
 				self.tree.addTerminal(t)
 				t = self.lexer.getNextToken()
 			elif term in self.grammar.tokens and term != t.type:
-				raise Exception("unexpected token "+t.type)
+				raise Exception("unexpected token "+t.type + " needed "+term)
 			else:
 				prev = self.tree.curr
 				self.tree.addTrans(term)
@@ -100,15 +104,64 @@ class Parser():
 				self.tree.curr = prev
 		return t;
 
-	def parse(self,root):
-		self.tree.addTrans(root)
+	def isCompilerTag(self,t):
+		if t.type == "include":
+			return True
+		elif t.type == "define":
+			return True
+		return False
+
+	def handleCompilerTag(self,t):
+		logging.debug("    ...Recognised Compiler Tag")
+		line = []
+		while t.type != "newline":
+			line.append(t)
+			t = l.getNextToken()
+		line.append(t)
+		self.compilerTags.append(line)
+		t = l.getNextToken()
+		return t
+	def handleComment(self,t):
+		logging.debug("    ...Recognised Comment")
+		comment = []
+		while t.type != "commentend":
+			comment.append(t)
+			t = l.getNextToken()
+		comment.append(t)
+		self.comments.append(comment)
+		t = l.getNextToken()
+		return t
+
+	def parse(self):
 		t = self.lexer.getNextToken()
-		self.decend(root,t)
+		# once this returns, keep decending into the next
+		# valid transformation until finished
+		while t != None:
+			if self.isCompilerTag(t):
+				t = self.handleCompilerTag(t)
+				continue
+			if t.type == "commentstart":
+				t = self.handleComment(t)
+				continue
+			nextTrans = None
+			logging.debug(" Look for next transformation for "+t.type)
+			for trans in self.grammar.transformations:
+				logging.debug("    ..."+trans)
+				if t.type in self.grammar.firstSets[trans]:
+					nextTrans = trans
+					break
+			if nextTrans == None:
+				raise Exception("No transformation found for "+t.type)
+			t = self.decend(nextTrans,t)
 
 if __name__ == "__main__":
+	# turn debugging on/off
+	logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+
+	# run the parser/lexer
 	l = Lexer("specs/C_Tokens_Simple.txt")
 	l.setInputStream("tests/test_basic.c")
 	p = Parser("specs/C_Grammar_Simple.txt",l)
-	p.parse("primary_expression")
+	p.parse()
 
 
